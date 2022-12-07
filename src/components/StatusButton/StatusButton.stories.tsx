6cbfac994e8137235e64cc0f6a4bb587f1a3713d
OrangeChick <retww@naver.com>
2022-12-07 18:26:59 +0900

    test: Change storybook test logic
    - 반복되는 storybook 테스트 코드를 수정하였습니다.

--- src/components/StatusButton/StatusButton.stories.tsx
+++ src/components/StatusButton/StatusButton.stories.tsx
@@ -12,67 +12,48 @@ const meta = {
       url: 'figmaURL',
     },
   },
+  args: { disabled: true },
 } as ComponentMeta<typeof StatusButton>;
-
-const disabled = true;
 
 export default meta;
 
-export const AgreeButton: ComponentStory<typeof StatusButton> = (args) => (
-  <StatusButton className="font-bold" {...args}>
+export const Agree: ComponentStory<typeof StatusButton> = ({
+  disabled,
+  ...args
+}) => (
+  <StatusButton className="font-bold" disabled={disabled} {...args}>
     확인
   </StatusButton>
 );
 
-export const DisabledAgreeButton: ComponentStory<typeof StatusButton> = (
-  args
-) => (
-  <StatusButton className="font-bold" disabled {...args}>
-    확인
-  </StatusButton>
-);
-
-export const PrivacyAgreementButton: ComponentStory<typeof StatusButton> = (
-  args
-) => (
+export const PrivacyAgreement: ComponentStory<typeof StatusButton> = ({
+  disabled,
+  ...args
+}) => (
   <StatusButton
-    className="relative pl-[102px] text-left text-body2 font-bold"
+    className={
+      disabled
+        ? 'text-body2 font-bold'
+        : 'relative pl-[102px] text-left text-body2 font-bold'
+    }
+    disabled={disabled}
     {...args}
   >
     <StatusButton.Icon
-      className="absolute top-5 left-[70px] h-6 w-6"
+      className={disabled ? 'hidden' : 'absolute top-5 left-[70px] h-6 w-6'}
       icon={CheckIcon}
     />
     개인정보 수집 및 이용동의
   </StatusButton>
 );
 
-export const DisabledPrivacyAgreementButton: ComponentStory<
-  typeof StatusButton
-> = (args) => (
-  <StatusButton className="text-body2 font-bold" disabled {...args}>
-    개인정보 수집 및 이용동의
-  </StatusButton>
-);
-
-export const BusSelectionButton: ComponentStory<typeof StatusButton> = (
-  args
-) => (
+export const BusSelection: ComponentStory<typeof StatusButton> = ({
+  disabled,
+  ...args
+}) => (
   <StatusButton
-    disabled={!disabled}
     className="h-12 bg-White pl-3 text-left"
-    {...args}
-  >
-    {!disabled ? '정류장을 먼저 선택해주세요' : '버스를 선택해주세요'}
-  </StatusButton>
-);
-
-export const DisabledBusSelectionButton: ComponentStory<typeof StatusButton> = (
-  args
-) => (
-  <StatusButton
     disabled={disabled}
-    className="h-12 bg-Gray-400 pl-3 text-left"
     {...args}
   >
     {disabled ? '정류장을 먼저 선택해주세요' : '버스를 선택해주세요'}
