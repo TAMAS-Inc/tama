f7b4abdf19c795b4ce6e235638d50ed7bc22ce72
loco9939 <kls9939@naver.com>
2022-12-08 16:59:46 +0900

    feat: create LandingLayout Page
    - 랜딩레이아웃 페이지 생성

---
+++ src/pages/Landing/LandingLayout.stories.tsx
@@ -0,0 +1,24 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { LandingLayout } from './LandingLayout';
+
+const meta = {
+  component: LandingLayout,
+  title: 'Directory/Page/LandingLayout',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'figmaURL',
+    },
+  },
+} as ComponentMeta<typeof LandingLayout>;
+
+export default meta;
+
+const Template: ComponentStory<typeof LandingLayout> = (args) => (
+  <LandingLayout {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {};
