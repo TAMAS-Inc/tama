39129f976c692d042a70abd996515beab9e025da
OrangeChick <retww@naver.com>
2022-12-08 20:04:11 +0900

    feat: Create MenuItem Component

---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,21 @@
+import { tw } from '@/utils/tailwindMerge';
+
+type MenuItemProps<T extends React.ElementType> = Component<T>;
+
+export function MenuItem({
+  children,
+  className,
+  ...restProps
+}: MenuItemProps<'div'>) {
+  return (
+    <div
+      className={tw(
+        'flex h-14 w-64 flex-col justify-center border-b border-Gray-100 bg-White pl-4',
+        className
+      )}
+      {...restProps}
+    >
+      {children}
+    </div>
+  );
+}
