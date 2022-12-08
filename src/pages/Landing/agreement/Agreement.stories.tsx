edb5e2f20a1b1576a3708d4169aad61e0576719e
loco9939 <kls9939@naver.com>
2022-12-08 17:00:37 +0900

    feat: create Agreement Page
    - 개인정보 동의 페이지 생성

---
+++ src/pages/Landing/agreement/Agreement.stories.tsx
@@ -0,0 +1,24 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Agreement } from './Agreement';
+
+const meta = {
+  component: Agreement,
+  title: 'Directory/Agreement',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'figmaURL',
+    },
+  },
+} as ComponentMeta<typeof Agreement>;
+
+export default meta;
+
+const Template: ComponentStory<typeof Agreement> = (args) => (
+  <Agreement {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {};
