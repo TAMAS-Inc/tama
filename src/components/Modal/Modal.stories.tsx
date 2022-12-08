b999ad1cf518236ae1ca0ede2453769a7cf94f9a
OrangeChick <retww@naver.com>
2022-12-08 15:03:08 +0900

    feat: Add DimBackground in Modal Component

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -11,22 +11,48 @@ const meta = {
       url: 'figmaURL',
     },
   },
+  args: {
+    isOpened: true,
+  },
 } as ComponentMeta<typeof Modal>;
 
 export default meta;
 
-export const Default: ComponentStory<typeof Modal> = (args) => (
-  <Modal className="w-[278px] rounded" {...args}>
-    <Modal.Content className="flex h-[120px] items-center justify-center rounded-tl rounded-tr text-center text-body2">
-      이 정류장을 삭제하시겠습니까?
-    </Modal.Content>
-    <Modal.Buttons>
-      <Modal.TextButton className="h-10 w-6/12 rounded-none rounded-bl border-r border-Gray-100 bg-Primary-300 text-body3 font-bold">
-        삭제
-      </Modal.TextButton>
-      <Modal.TextButton className="h-10 w-6/12 rounded-none rounded-br bg-Primary-300 text-body3 font-bold">
-        취소
-      </Modal.TextButton>
-    </Modal.Buttons>
+export const Delete: ComponentStory<typeof Modal> = ({ isOpened, ...args }) => (
+  <Modal isOpened={isOpened} {...args}>
+    <Modal.DimBackground />
+    <Modal.ModalContainer>
+      <Modal.Content className="bg-White">
+        이 정류장을 삭제하시겠습니까?
+      </Modal.Content>
+      <Modal.Buttons>
+        <Modal.TextButton className="h-10 w-6/12 rounded-none rounded-bl border-r border-Gray-100 bg-Primary-300">
+          삭제
+        </Modal.TextButton>
+        <Modal.TextButton className="h-10 w-6/12 rounded-none rounded-br bg-Primary-300">
+          취소
+        </Modal.TextButton>
+      </Modal.Buttons>
+    </Modal.ModalContainer>
+  </Modal>
+);
+
+export const Inquiry: ComponentStory<typeof Modal> = ({
+  isOpened,
+  ...args
+}) => (
+  <Modal isOpened={isOpened} {...args}>
+    <Modal.DimBackground />
+    <Modal.ModalContainer>
+      <Modal.Content className="flex flex-col bg-White ">
+        <p>정상적으로 처리되었습니다.</p>
+        <p>감사합니다.</p>
+      </Modal.Content>
+      <Modal.Buttons>
+        <Modal.TextButton className="h-10 w-full rounded-none rounded-br rounded-bl bg-Primary-300">
+          확인
+        </Modal.TextButton>
+      </Modal.Buttons>
+    </Modal.ModalContainer>
   </Modal>
 );
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,15 +1,75 @@
+import { useMemo, createContext } from 'react';
 import { tw } from '@/utils/tailwindMerge';
 import { TextButton } from '../TextButton/TextButton';
+import { DimBackground } from '../DimBackground/DimBackground';
 
-type ModalProps<T extends React.ElementType> = Component<T>;
+interface ModalContextValue {
+  isOpened: boolean | undefined;
+}
+
+const ModalContext = createContext<ModalContextValue | null>(null);
+
+type ModalProps<T extends React.ElementType> = {
+  isOpened?: boolean;
+} & Component<T>;
 
 export function Modal({
   children,
   className,
+  isOpened,
   ...restProps
 }: ModalProps<'div'>) {
+  const ctxValue = useMemo((): ModalContextValue => ({ isOpened }), [isOpened]);
+
   return (
-    <div className={tw('', className)} {...restProps}>
+    <ModalContext.Provider value={ctxValue}>
+      <div
+        className={tw(
+          'relative h-[844px] w-[390px]',
+          !isOpened && 'hidden',
+          className
+        )}
+        {...restProps}
+      >
+        {children}
+      </div>
+    </ModalContext.Provider>
+  );
+}
+
+type ModlaContainerProps<T extends React.ElementType> = Component<T>;
+
+function ModlaContainer({
+  children,
+  className,
+  ...restProps
+}: ModlaContainerProps<'div'>) {
+  return (
+    <div
+      className={tw(
+        'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+        className
+      )}
+      {...restProps}
+    >
+      {children}
+    </div>
+  );
+}
+
+Modal.ModalContainer = ModlaContainer;
+
+type ContentProps<T extends React.ElementType> = Component<T>;
+
+function Content({ children, className, ...restProps }: ContentProps<'div'>) {
+  return (
+    <div
+      className={tw(
+        'flex h-[120px] w-[278px] items-center justify-center rounded-tl rounded-tr text-center text-body2',
+        className
+      )}
+      {...restProps}
+    >
       {children}
     </div>
   );
@@ -19,22 +79,16 @@ type ButtonsProps<T extends React.ElementType> = Component<T>;
 
 function Buttons({ children, className, ...restProps }: ButtonsProps<'div'>) {
   return (
-    <div className={tw('', className)} {...restProps}>
+    <div
+      className={tw('w-[278px] text-body3 font-bold', className)}
+      {...restProps}
+    >
       {children}
     </div>
   );
 }
 
-type ContentProps<T extends React.ElementType> = Component<T>;
-
-function Content({ children, className, ...restProps }: ContentProps<'div'>) {
-  return (
-    <div className={tw('', className)} {...restProps}>
-      {children}
-    </div>
-  );
-}
-
+Modal.DimBackground = DimBackground;
 Modal.Content = Content;
 Modal.Buttons = Buttons;
 Modal.TextButton = TextButton;
