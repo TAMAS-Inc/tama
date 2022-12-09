1f12edd2f274d8f81b1de54b680b5ff48e522b63
loco9939 <kls9939@naver.com>
2022-12-09 13:42:30 +0900

    feat: create AgreementPage
    - 동의 수집 페이지 로직 완성

--- src/pages/Landing/agreement/Agreement.tsx
+++ src/pages/Landing/agreement/Agreement.tsx
@@ -1,7 +1,5 @@
-import { useState, ComponentProps } from 'react';
-import { CheckCircleIcon } from '@heroicons/react/24/solid';
-import { CheckCircleIcon as CheckCircleIconOff } from '@heroicons/react/24/outline';
-import { ToggleIconButton } from '@/components';
+import { useState, ComponentProps, ChangeEvent, useEffect } from 'react';
+import { InputContainer } from '@/components';
 import { LandingLayout } from '../LandingLayout';
 
 type AgreementProps<T extends React.ElementType> = ComponentProps<
@@ -14,58 +12,71 @@ export function Agreement({
   className,
   ...restProps
 }: AgreementProps<'div'>) {
-  const [entireAgree, setEntireAgree] = useState(false);
-  const [locationAgree, setLocationAgree] = useState(false);
-  const [adAgree, setAdAgree] = useState(false);
-  const [isAgree, setIsAgree] = useState(false);
+  const [checkList, setCheckList] = useState<string[]>([]);
+  const [isAgree, setIsAgree] = useState<boolean>(false);
 
-  // 어떤 조건이 만족했을 때, 동의가 false에서 true로 변경되어야 한다.
+  const checkAll = (e: ChangeEvent<HTMLInputElement>) => {
+    e.target.checked ? setCheckList(['location', 'ad']) : setCheckList([]);
+  };
+
+  const check = (e: ChangeEvent<HTMLInputElement>) => {
+    e.target.checked
+      ? setCheckList([...checkList, e.target.name])
+      : setCheckList(checkList.filter((choice) => choice !== e.target.name));
+  };
+
+  useEffect(() => {
+    if (checkList.includes('location') || checkList.length === 2) {
+      setIsAgree(true);
+    } else {
+      setIsAgree(false);
+    }
+  });
 
   return (
+    // 페이지 코드가 길다 => 좀 더 리액트스럽게 만들어보도록 하자
     <LandingLayout agree={isAgree} className="pl-4 pr-4" {...restProps}>
       <p className="mt-24 pb-12">
         만나서 반갑습니다. 아래 약관에 동의하시면 행복한 출근길이 시작될 거예요!
       </p>
-      <div
-        className="border-bottom flex border-b border-b-Gray-400 pb-4"
-        onClick={() => {
-          setEntireAgree(!entireAgree);
-        }}
-        aria-hidden="true"
-      >
-        <ToggleIconButton className="ml-2 mr-2">
-          <ToggleIconButton.Icon
-            icon={entireAgree ? CheckCircleIcon : CheckCircleIconOff}
+      <InputContainer className="border-bottom flex rounded-none border-b border-b-Gray-400 pb-4">
+        <InputContainer.Label className="font-bold">
+          <InputContainer.Label.Input
+            name="all"
+            type="checkbox"
+            className="mx-2 h-6 w-6 bg-Gray-500"
+            onChange={checkAll}
+            checked={checkList.length === 2}
           />
-        </ToggleIconButton>
-        <span className="font-bold">전체 동의</span>
-      </div>
-      <div
-        className="border-bottom mt-4 flex pb-4"
-        onClick={() => {
-          setLocationAgree(!locationAgree);
-        }}
-        aria-hidden="true"
-      >
-        <ToggleIconButton className="ml-2 mr-2">
-          <ToggleIconButton.Icon
-            icon={locationAgree ? CheckCircleIcon : CheckCircleIconOff}
+          전체 동의
+        </InputContainer.Label>
+      </InputContainer>
+
+      <InputContainer className="mt-4 flex rounded-none">
+        <InputContainer.Label>
+          <InputContainer.Label.Input
+            name="location"
+            type="checkbox"
+            className="mx-2 h-6 w-6 bg-Gray-500"
+            onChange={check}
+            checked={checkList.includes('location')}
           />
-        </ToggleIconButton>
-        <span className="">위치 기반 서비스 약관 동의 (필수)</span>
-      </div>
-      <div
-        className="border-bottom flex pb-4"
-        onClick={() => setAdAgree(!adAgree)}
-        aria-hidden="true"
-      >
-        <ToggleIconButton className="ml-2 mr-2">
-          <ToggleIconButton.Icon
-            icon={adAgree ? CheckCircleIcon : CheckCircleIconOff}
+          위치 기반 서비스 약관 동의 (필수)
+        </InputContainer.Label>
+      </InputContainer>
+
+      <InputContainer className="mt-4 flex rounded-none">
+        <InputContainer.Label>
+          <InputContainer.Label.Input
+            name="ad"
+            type="checkbox"
+            className="mx-2 h-6 w-6 bg-Gray-500"
+            onChange={check}
+            checked={checkList.includes('ad')}
           />
-        </ToggleIconButton>
-        <span className="">마케팅 정보 수신 동의 (선택)</span>
-      </div>
+          마케팅 정보 수신 동의 (선택)
+        </InputContainer.Label>
+      </InputContainer>
     </LandingLayout>
   );
 }
