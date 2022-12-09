b8e436e8de79358f324f046a33c77531c3b0e5b3
loco9939 <kls9939@naver.com>
2022-12-09 18:11:28 +0900

    fix: remove Icon props
    - 아이콘 props 제거 (기본값 있어요~)

--- src/pages/Landing/searchBusStop/SearchBusStop.tsx
+++ src/pages/Landing/searchBusStop/SearchBusStop.tsx
@@ -1,6 +1,5 @@
-import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 import { tw } from '@/utils/tailwindMerge';
-import { InputContainer, Icon } from '@/components';
+import { InputContainer, NavigationHeader } from '@/components';
 
 type SearchBusStopProps<T extends React.ElementType> = Component<T>;
 
@@ -13,18 +12,17 @@ export function SearchBusStop({
       className={tw('mt-8 flex items-center pl-4 pr-4', className)}
       {...restProps}
     >
-      <Icon
-        icon={ChevronLeftIcon}
-        className="h-6 w-6 text-Gray-400"
-        stroke-width="2"
-      />
-      <InputContainer className="relative h-12 w-full pl-3">
-        <InputContainer.Input
-          className="bg-Gray-100"
-          placeholder="정류장 검색"
-        />
-        <InputContainer.ResetButton className="absolute top-3 right-3 h-6 w-6 fill-Gray-100" />
-      </InputContainer>
+      <NavigationHeader className="-ml-4">
+        <InputContainer className="relative h-12 w-full pl-3">
+          <InputContainer.Label>
+            <InputContainer.Label.Input
+              className="bg-Gray-100"
+              placeholder="정류장 검색"
+            />
+          </InputContainer.Label>
+          <InputContainer.ResetButton className="absolute top-3 right-3 h-6 w-6 fill-Gray-500" />
+        </InputContainer>
+      </NavigationHeader>
     </div>
   );
 }
