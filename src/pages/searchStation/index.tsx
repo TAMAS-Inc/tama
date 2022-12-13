2f5ad23e447c5e92d3fc42704473294482cde451
loco9939 <kls9939@naver.com>
2022-12-13 15:46:43 +0900

    feat: Create SearchStation Page

--- src/pages/searchStation/index.tsx
+++ src/pages/searchStation/index.tsx
@@ -1,16 +1,24 @@
 import { useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import { Header, InputContainer, List, ToggleIconButton } from '@/components';
+import { Header, InputContainer, List, StatusButton } from '@/components';
 import { tw } from '@/utils/tailwindMerge';
 import { BaseModal } from '@/components/BaseModal';
 
 type SearchBusStopProps<T extends React.ElementType> = Component<T>;
+type SelectedStation = { title: string; subtitle: string };
+type Location = {
+  hash: string;
+  key: string;
+  pathname: string;
+  search: string;
+  state: { userStation: string };
+};
 
 const stations = [
-  { title: '기흥여객차고지', subtitle: 500095 },
-  { title: '기흥역', subtitle: 511095 },
-  { title: '기흥차고지', subtitle: 52295 },
+  { title: '기흥여객차고지', subtitle: '500095' },
+  { title: '기흥역', subtitle: '511095' },
+  { title: '기흥차고지', subtitle: '52295' },
 ];
 const buses = [
   {
@@ -24,33 +32,45 @@ export default function SearchBusStop({
   className,
   ...restProps
 }: SearchBusStopProps<'div'>) {
-  const location = useLocation();
-  const { station } = location.state;
+  const location: Location = useLocation();
+  const { userStation } = location.state;
 
-  const [selectedStation, setSelectedStation] = useState<string>('');
-  const [selectedStationInfo, setSelectedStationInfo] = useState<string>('');
+  const [selectedBus, setSelectedBus] = useState<SelectedStation['title'][]>(
+    []
+  );
+  const [selectedStation, setSelectedStation] =
+    useState<SelectedStation | null>(null);
+
   const [showTip, setShowTip] = useState<boolean>(true);
   const [isOpen, setIsOpen] = useState<boolean | null>(null);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleClick = (e) => {
+  const handleCloseClick = () => setIsOpen(false);
+  const handleResetClick = () => setShowTip(true);
+  const handleClick = ({ title, subtitle }: SelectedStation) => {
     setIsOpen(true);
-    setSelectedStation(e.target.value);
+    setSelectedStation((prevStation) => ({
+      ...prevStation,
+      title,
+      subtitle,
+    }));
   };
 
-  const handleChange = () => {
-    setShowTip(inputRef.current?.value === '');
-  };
-
-  const handleResetClick = () => {
-    setShowTip(true);
-  };
+  const handleChange = () => setShowTip(inputRef.current?.value === '');
+  const handleChangeCheckbox = (title: SelectedStation['title']) =>
+    !selectedBus.includes(title)
+      ? setSelectedBus((prevBus) => [...prevBus, title])
+      : setSelectedBus((prevBus) => [
+          ...prevBus.filter((busTitle) => busTitle !== title),
+        ]);
 
   return (
     <div className={tw('mt-8 w-full', className)} {...restProps}>
       <Header>
-        <Header.BackButton />
+        <Link to="/commute" state={{ userStation }}>
+          <Header.BackButton />
+        </Link>
         <Header.Title className="grow">
           <InputContainer className="relative w-full pl-3 ">
             <InputContainer.Label>
@@ -83,15 +103,8 @@ export default function SearchBusStop({
         <List className="mt-9 ml-4">
           {stations.map(({ title, subtitle }) => (
             <List.Item
-              onClick={(e) => {
-                setIsOpen(true);
-                setSelectedStationInfo(
-                  e.target.closest('button').lastElementChild.textContent
-                );
-                setSelectedStation(
-                  e.target.closest('button').firstElementChild.textContent
-                );
-              }}
+              key={title}
+              onClick={() => handleClick({ title, subtitle })}
               className="pl-2"
             >
               <List.Title>{title}</List.Title>
@@ -104,22 +117,43 @@ export default function SearchBusStop({
         <BaseModal>
           <BaseModal.Content className="top-56 h-full w-full rounded-t-2xl bg-White">
             <List>
-              <List.Item>
-                <List.Title>{selectedStation}</List.Title>
-                <List.Subtitle>{selectedStationInfo}</List.Subtitle>
+              <List.Item className="pl-6">
+                <List.Title>{selectedStation?.title}</List.Title>
+                <List.Subtitle>{selectedStation?.subtitle}</List.Subtitle>
                 <List.Icon icon={ChevronDownIcon} />
               </List.Item>
 
-              {buses.map(({ title, subtitle }) => (
-                <List.Item key={title} className="pl-2">
-                  <List.Title>{title}</List.Title>
-                  <List.Subtitle>{subtitle} | 종점방면</List.Subtitle>
-                  <ToggleIconButton />
+              {buses.map(({ title }) => (
+                <List.Item key={title} className="relative pl-4">
+                  <List.Title className="pl-2 text-body1 text-Primary-700">
+                    {title}
+                  </List.Title>
+                  <InputContainer className="absolute top-6 right-6 h-6 w-6">
+                    <InputContainer.Label>
+                      <InputContainer.Label.Input
+                        onChange={() => handleChangeCheckbox(title)}
+                        type="checkbox"
+                        className="bg-Gray-500"
+                      />
+                    </InputContainer.Label>
+                  </InputContainer>
                 </List.Item>
               ))}
             </List>
+            <Link
+              to="/commute"
+              state={{
+                userStation,
+                stationName: selectedStation?.title,
+                bus: selectedBus,
+              }}
+            >
+              <StatusButton className="fixed left-4 bottom-8 w-[calc(100%-32px)]">
+                확인
+              </StatusButton>
+            </Link>
           </BaseModal.Content>
-          <BaseModal.DimBg onClick={() => setIsOpen(false)} />
+          <BaseModal.DimBg onClick={handleCloseClick} />
         </BaseModal>
       )}
     </div>
