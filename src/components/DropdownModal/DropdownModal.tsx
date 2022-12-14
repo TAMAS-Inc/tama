36dd2eda3d350b11e991d034653e5c25afb98826
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-12-14 16:46:15 +0900

    feat: Add Chart using rechart.js - API should be changed and chart logic will be changed - +Dropdown border rounded

--- src/components/DropdownModal/DropdownModal.tsx
+++ src/components/DropdownModal/DropdownModal.tsx
@@ -21,19 +21,19 @@ export function DropdownModal({
       routeId: '1',
       routeName: '춘시기네',
       stationName: '기흥역',
-      dest: '롯데캐슬스카이, 이안두드림, 백남준아트센터 방면',
+      routeList: '5001A',
     },
     {
       routeId: '2',
       routeName: '라이언네',
       stationName: '보정역',
-      dest: '롯데캐슬스카이, 이안두드림, 백남준아트센터 방면',
+      routeList: '5001B, 5003',
     },
     {
       routeId: '3',
       routeName: '이주네',
       stationName: '미금역',
-      dest: '롯데캐슬스카이, 이안두드림, 백남준아트센터 방면',
+      routeList: '8000, 9000',
     },
   ];
 
@@ -49,56 +49,50 @@ export function DropdownModal({
   };
 
   return (
-    <BaseModal className={tw('pl-0 ', className)} {...restProps}>
-      <BaseModal.Content className="bottom-0 h-[620px] w-full bg-White">
-        <List className="relative">
-          <Link to="/">
-            <TextButton className="absolute top-3 right-8 z-10 bg-White text-body2">
-              편집
-            </TextButton>
-          </Link>
-          <div className="pt-3">
-            <List.Title className="text-center text-body1 font-bold">
-              내 정류장 설정
-            </List.Title>
-          </div>
-          <Link to="/" className="block text-center">
-            <TextButton className="mt-3.5 mb-8 h-8 w-[320px] text-left text-body2 text-Gray-500">
-              정류장 검색
-            </TextButton>
-          </Link>
+    <BaseModal className={tw('', className)} {...restProps}>
+      <BaseModal.Content className="bottom-0 h-[620px] w-full overflow-hidden rounded-lg bg-White">
+        <div className="flex flex-row justify-between pt-8 pl-6 pr-8 pb-8">
+          <div className="text-body1 font-bold">내 정류장 설정</div>
+          <ul className="flex gap-2 text-body2">
+            <li>
+              <Link to="/commute">추가</Link>
+            </li>
+            <li>
+              <Link to="/commute/edit">편집</Link>
+            </li>
+          </ul>
+        </div>
 
-          {data.map(({ routeId, routeName, stationName, dest }) => (
-            // <MyStationCard
-            //   key={routeName}
-            //   routeName={routeName}
-            //   stationName={stationName}
-            //   dest={dest}
-            //   id={`${i}`}
-            //   handleSelect={handleSelect}
-            //   selected={+i === selected}
-            // />
-            <BusCard
-              key={routeId}
-              id={routeName}
-              className="flex flex-col items-start justify-center"
-              onClick={handleSelect}
-            >
-              <BusCard.Info className="static left-0 flex translate-x-0 flex-row">
-                <BusCard.Content className="w-28 text-body1">
-                  {routeName}
-                </BusCard.Content>
-                <BusCard.StationName className="mb-0 flex items-center justify-center">
-                  {stationName}
-                </BusCard.StationName>
-              </BusCard.Info>
-              <BusCard.Content className="text-Gray-400">
-                {dest}
+        {data.map(({ routeId, routeName, stationName, routeList }) => (
+          // <MyStationCard
+          //   key={routeName}
+          //   routeName={routeName}
+          //   stationName={stationName}
+          //   routeList={routeList}
+          //   id={`${i}`}
+          //   handleSelect={handleSelect}
+          //   selected={+i === selected}
+          // />
+          <BusCard
+            key={routeId}
+            id={routeName}
+            className="flex flex-col items-start justify-center"
+            onClick={handleSelect}
+          >
+            <BusCard.Info className="static left-0 flex translate-x-0 flex-row">
+              <BusCard.Content className="w-28 text-body1">
+                {routeName}
               </BusCard.Content>
-              {routeName === selected && <BusCard.CheckIcon isChecked />}
-            </BusCard>
-          ))}
-        </List>
+              <BusCard.StationName className="mb-0 flex items-center justify-center">
+                {stationName}
+              </BusCard.StationName>
+            </BusCard.Info>
+            <BusCard.Content className="text-Gray-400">
+              {routeList}
+            </BusCard.Content>
+            {routeName === selected && <BusCard.CheckIcon isChecked />}
+          </BusCard>
+        ))}
       </BaseModal.Content>
       <BaseModal.DimBg onClick={handleClickDimmed} />
     </BaseModal>
--- src/pages/Main/Analysis/[id].tsx
+++ src/pages/Main/Analysis/[id].tsx
@@ -1,28 +1,132 @@
+import {
+  ComposedChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  LabelList,
+} from 'recharts';
 import { tw } from '@/utils/tailwindMerge';
 import { AD, NavigationHeader, Notification, SyncButton } from '@/components';
 
+const CHART_LENGTH = 4;
+
 type AnalysisProps<T extends React.ElementType> = Component<T>;
+interface PredictInfo {
+  stationId: string;
+  stationName: string;
+  remainStationCnt: number;
+  predictRemainSeatCnt: number;
+}
+
+interface CurrentInfo extends PredictInfo {
+  remainSeatCnt: number;
+}
+
+interface AnalysisInfo {
+  current: CurrentInfo;
+  predict: PredictInfo[];
+}
 
 export default function Analysis({
   className,
   ...restProps
 }: AnalysisProps<'div'>) {
+  const data = {
+    current: {
+      stationId: '228000187',
+      stationName: '남동구미마을',
+      remainStationCnt: 5,
+      predictRemainSeatCnt: 30,
+      remainSeatCnt: 28,
+    },
+    predict: [
+      {
+        stationId: '228000191',
+        stationName: '명지대',
+        remainStationCnt: 0,
+        predictRemainSeatCnt: 3,
+      },
+      {
+        stationId: '228002958',
+        stationName: '명지대앞',
+        remainStationCnt: 1,
+        predictRemainSeatCnt: 10,
+      },
+      {
+        stationId: '228000190',
+        stationName: '동진마을',
+        remainStationCnt: 2,
+        predictRemainSeatCnt: 17,
+      },
+    ],
+  };
+
+  const getUpdatedPredictSeatCnt = (src: number, diff: number): number =>
+    src - diff > 0 ? src - diff : 0;
+
+  const getPredictedUpdatedData = ({ current, predict }: AnalysisInfo) => {
+    const diff = current.predictRemainSeatCnt - current.remainSeatCnt;
+    const sortedPredict = predict.sort(
+      (a, b) => b.predictRemainSeatCnt - a.predictRemainSeatCnt
+    );
+
+    return {
+      current,
+      predict: [
+        ...sortedPredict.map((station) => ({
+          ...station,
+
+          predictRemainSeatCnt: getUpdatedPredictSeatCnt(
+            station.predictRemainSeatCnt,
+            diff
+          ),
+        })),
+      ],
+    };
+  };
+
+  const getChartData = ({ current, predict }: AnalysisInfo) => ({
+    isWaveChart: current.remainStationCnt > CHART_LENGTH,
+    data: [
+      { ...current, label: `${current.remainStationCnt}번째 전` },
+      ...predict.map((station) => ({
+        ...station,
+        label:
+          station.remainStationCnt === 0
+            ? station.stationName
+            : `${station.remainStationCnt}번째 전`,
+      })),
+    ],
+  });
+
+  const updatedData = getPredictedUpdatedData(data);
+  const chartData = getChartData(updatedData);
+
   return (
     <div className={tw('pt-8', className)} {...restProps}>
       <NavigationHeader>실시간 분석</NavigationHeader>
       <Notification />
-
-      <div className="flex flex-col gap-4 pt-8 pl-7 text-body1 font-bold">
+      <div className="flex flex-col gap-4 pt-8 pl-7 pr-7 text-body1 font-bold">
         <p>
-          <strong className="font-bold text-Primary-600">기흥역</strong> 에서
+          <strong className="font-bold text-Primary-600">
+            {data.predict[0].stationName}
+          </strong>
+          에서
           <br />
           <strong className="font-bold text-Primary-600">5001번</strong> 버스의
           예상 잔여 좌석은
           <br />
-          <strong className="font-bold text-Primary-600">0석</strong> 일 것
-          같아요!
+          <strong className="font-bold text-Primary-600">
+            {updatedData.predict.at(-1)?.predictRemainSeatCnt}석
+          </strong>{' '}
+          일 것 같아요!
         </p>
-        <p>~차트~</p>
+        <Analysis.Chart
+          data={chartData.data}
+          isWaveChart={chartData.isWaveChart}
+        />
         <p>
           사용자의 <strong className="font-bold text-Primary-600">90%</strong>가
           만족하고 있어요!
@@ -33,3 +137,80 @@ export default function Analysis({
     </div>
   );
 }
+
+function Chart({
+  data,
+  isWaveChart,
+}: {
+  data: (CurrentInfo | PredictInfo)[];
+  isWaveChart: boolean;
+}) {
+  return (
+    <ComposedChart
+      width={350}
+      height={400}
+      data={data}
+      margin={{
+        left: -30,
+      }}
+    >
+      <CartesianGrid stroke="#f5f5f5" />
+      <XAxis dataKey="label" fontSize="12px" />
+      {isWaveChart && (
+        <>
+          <line
+            strokeDasharray="5 5"
+            x1="130"
+            y1="340"
+            x2="130"
+            y2="0"
+            className="mr-5 stroke-Black"
+          />
+          <text
+            offset="5"
+            x="120"
+            y="175"
+            className="fill-Gray-500 text-body3"
+            textAnchor="middle"
+          >
+            <tspan x="105" y="50">
+              중략
+            </tspan>
+          </text>
+        </>
+      )}
+
+      <YAxis fontSize="12px" />
+      <Legend
+        wrapperStyle={{ fontSize: '16px' }}
+        verticalAlign="bottom"
+        height={20}
+      />
+      <Bar
+        name="실시간 좌석"
+        dataKey="remainSeatCnt"
+        barSize={20}
+        fontSize={5}
+        className="fill-Primary-500"
+      >
+        <LabelList fontSize="14px" dataKey="remainSeatCnt" position="top" />
+      </Bar>
+      <Bar
+        name="예측 좌석"
+        type="monotone"
+        barSize={20}
+        dataKey="predictRemainSeatCnt"
+        className="fill-Gray-300"
+      >
+        <LabelList
+          fontSize="14px"
+          className="fill-Gray-300"
+          dataKey="predictRemainSeatCnt"
+          position="top"
+        />
+      </Bar>
+    </ComposedChart>
+  );
+}
+
+Analysis.Chart = Chart;
