14544a7891143d0a941adbddc132151ec87806f3
loco9939 <kls9939@naver.com>
2022-12-20 20:28:59 +0900

    feat: Add focusing logic on ResetButton
    - 검색 페이지에서 ResetButton 포커스 기능 추가
    - 로컬 서버 다른 디바이스에서도 추가할 수 있도록 config 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { host: true },
   resolve: {
     alias: {
       '@': path.resolve('./src'),
